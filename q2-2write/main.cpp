bc4d6ecea2e12eac1f7d2f50077cb71697f184a3
Louisa Taufaasau <taufaasau98@gmail.com>
2021-10-19 15:55:25 -0700

    .

--- q2-2write/main.cpp
+++ q2-2write/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 struct student{
-    int idNumber,score[2], totalscore;
+    int idNumber,score[3], totalscore;
     char name[10];
     double average;
 };
@@ -15,19 +15,20 @@ void outputBinary(student *x) {
 }
 
 int main(){
-    int studentID, score1, score2;
+    int studentID, score1, score2, score3;
     char name[10];
     fstream file;
     file.open("students.txt");
     
     for (int i=0; i<10; i++){
         student *students = new student;
-        file >> studentID >> name >> score1 >> score2;
+        file >> studentID >> name >> score1 >> score2 >> score3;
         students->idNumber=studentID;
         students->score[0]=score1;
         students->score[1]=score2;
-        students->average=(score1+score2)/2;
-        students->totalscore=score1+score2;
+        students->score[2]=score3;
+        students->average=(score1+score2+score3)/3;
+        students->totalscore=score1+score2+score3;
         strcpy(students->name, name);
         outputBinary(students);
     }
