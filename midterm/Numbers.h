b966c2c59916f63517be5df4cfff874ea6d0ce0f
Louisa Taufaasau <taufaasau98@gmail.com>
2021-11-30 10:39:12 -0800

    first

---
+++ midterm/Numbers.h
@@ -0,0 +1,22 @@
+//
+//  Numbers.h
+//  Midterm
+//
+//  Created by Louisa Taufaasau on 11/30/21.
+//
+
+#ifndef Numbers_h
+#define Numbers_h
+class Numbers{
+private:
+    int ID;
+    vector<T> values;
+    static int NumofObjects;
+public:
+    static int getNumObjects(void);
+    
+}
+
+int Numbers::NumofObjects = 0;
+
+#endif /* Numbers_h */
