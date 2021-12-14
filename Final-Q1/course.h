55ca340e672abac82a6b4adc9dec6420be337d2c
Louisa Taufaasau <taufaasau98@gmail.com>
2021-12-13 17:44:25 -0800

    start

---
+++ Final-Q1/course.h
@@ -0,0 +1,14 @@
+#ifndef course_h
+#define course_h
+
+class Course{
+private:
+    int ID;
+    string name;
+    int credit;
+public:
+    Course();
+   // Constructor and Member functions that would be necessary to be used in this question
+}
+
+#endif /* course_h */
