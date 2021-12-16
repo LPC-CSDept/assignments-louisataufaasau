dceefe79487e42318e9a47d2ea28d528fcad0fe6
Louisa Taufaasau <taufaasau98@gmail.com>
2021-12-16 15:41:37 -0800

    quiz 4 update

--- quiz 4/main.cpp
+++ quiz 4/main.cpp
@@ -17,35 +17,14 @@ int main() {
     ifs.close();
     
     ifs.open("addstudents.txt");
-
-    while (!ifs.eof()) {
-
-        string cname;
-        int size, course_id=-1;
-        
-        ifs >> cname >> size;
-        
-        for (int i=0; i<courses[0].getNUM_COURSES(); i++) {
-            if (cname == courses[i].getCname()) {
-                course_id = i;
-            }
-        }
-
-        for (int i=0; i<size; i++) {
-            int idd;
-            string s;
-            char g;
-            double ss;
-
-            ifs >> idd >> s >> g >> ss;
-            Student student(idd, s, g, ss);
-            courses[course_id].addStudent(student);
-        }
-
-        cout << "Updated Students list for the Course " << cname << endl;
-        cout << courses[course_id] << endl;
-           
-   }
+    
+    cout << "Updating courses." << endl;
+    for (int i=0; i<courses[0].getNUM_COURSES(); i++){
+        ifs >> courses[i];
+        cout << courses[i] << endl;
+    }
+    
+    ifs.close();
     
     cout << "Printing all courses" << endl;
     for (int i=0; i<courses[0].getNUM_COURSES(); i++) {
